6b916633a595827f2d004586e1981db4617d888f
oasc14 <77084822+oasc14@users.noreply.github.com>
2023-07-13 01:09:33 +0000

    Added dynamic data and API route

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+PRODUCTOS = [
+  {
+    'id':1,
+    'title': 'Flores',
+    'description': 'Sativa, Indica e Híbridas'
+    
+  },
+  {
+    'id':2,
+    'title': 'Vapeadores',
+    'description': 'Extracción y Líquido'
+    
+  },
+  {
+    'id':3,
+    'title': 'Comestibles',
+    'description': 'Galletas, Dulces y más'
+    
+  }
+  
+]
+
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', productos=PRODUCTOS)
+
+@app.route('/producto')
+def list_products():
+  return jsonify(PRODUCTOS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
